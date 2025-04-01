309530f2c3df95632798c6f9383f0b0469440b2a
helin sara baran <sarabaran2311@gmail.com>
2025-04-01 21:51:34 +0200

    Tilføjer kommentar

--- Pages/Index1.cshtml.cs
+++ Pages/Index1.cshtml.cs
@@ -1,12 +1,24 @@
+using EasyBook.Repository;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
 
 namespace EasyBook.Pages
-{
+{// vi har oprettet en razor page/index1.cshtml
     public class Index1Model : PageModel
     {
-        public void OnGet()
+        // Instanserer et objekt af MeetingRoomRepository, dette bruges til at hente mødelokalerne
+        // vi gøre brug af readonly for at sikre at objektet ikke bliver ændret
+        private readonly MeetingRoomRepository _meetingRoomRepo = new MeetingRoomRepository();
+
+        // vi opretter en liste til at holde mødelokaler
+        public List<MeetingRoom> MeetingRooms { get; set; }
+
+        // vi opretter en metode der hedder OnGet
+        public void OnGet() 
         {
+            // Vi henter alle mødelokalerne fra MeetingRoomRepository
+            MeetingRooms = _meetingRoomRepo.GetAll();
         }
     }
 }
