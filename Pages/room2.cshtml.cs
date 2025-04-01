382c73522dc92bad2f1943fd675d4551b51a9918
Ahmed Hemdan <aha007@edu.zealand.dk>
2025-04-01 13:22:24 +0200

    roomss

---
+++ Pages/room2.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EasyBook.Pages
+{
+    public class room2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/room5.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EasyBook.Pages
+{
+    public class room5Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
