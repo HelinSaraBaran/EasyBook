e63e9a5e8d745e1e30671ba3fb4badf9553f687d
helin sara baran <sarabaran2311@gmail.com>
2025-04-01 22:53:33 +0200

    fix

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+using EasyBook.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EasyBook.Pages
+{
+    public class IndexModel : PageModel
+    {
+        // vi opretter en instans af MeetingRoomRepository til at hente mødelokalerne
+        private readonly MeetingRoomRepository _meetingRoomRepo = new MeetingRoomRepository();
+
+        // vi laver en "offentlig" liste af mødelokaler
+        public List<MeetingRoom> MeetingRooms { get; set; }
+
+        // Vi opretter en metode der hedder OnGet
+        public void OnGet()
+        {
+            // vi henter alle mødelokalerne
+            MeetingRooms = _meetingRoomRepo.GetAll();
+        }
+    }
+}
