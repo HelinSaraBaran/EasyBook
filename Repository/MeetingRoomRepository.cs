ca48a7ff65b9e0bc54a517a2e1d2b2baabb3188a
FL03NG <sih013@edu.zealand.dk>
2025-04-02 10:40:07 +0200

    jho

--- Repository/MeetingRoomRepository.cs
+++ Repository/MeetingRoomRepository.cs
@@ -8,14 +8,11 @@ namespace EasyBook.Repository
         // vi opretter en liste af mødelokaler
         private List<MeetingRoom> meetingRooms = new List<MeetingRoom>
         {
-        new MeetingRoom {Id = 1, Name = "Mødelokale A", Capacity = 10, Equipment = "Projektor",
-        IsAvailable = true },
+        new MeetingRoom {Id = 1, Name = "Mødelokale A", Capacity = 10, IsAvailable = true, Whiteboard = true, Projector = true, ImagePath = "" },
 
-        new MeetingRoom {Id = 2, Name = "Mødelokale B", Capacity = 20, Equipment = "Whiteboard",
-        IsAvailable = true },
+        new MeetingRoom {Id = 2, Name = "Mødelokale B", Capacity = 20, IsAvailable = true, Whiteboard = true, Projector = true, ImagePath = "" },
 
-        new MeetingRoom { Id = 3, Name = "Mødelokale C", Capacity = 30, Equipment = "Projektor",
-            IsAvailable = true }
+        new MeetingRoom {Id = 3, Name = "Mødelokale C", Capacity = 30, IsAvailable = true, Whiteboard = true, Projector = true, ImagePath = "" }
         };
         // metode til at hente alle mødelokalerne
         public List<MeetingRoom> GetAll() => meetingRooms;
