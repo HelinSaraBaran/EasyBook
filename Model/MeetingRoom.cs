ac15a2dba4ae6c3d8dcdddfa2c653e248f543c8f
Ahmed Hemdan <aha007@edu.zealand.dk>
2025-04-02 10:23:02 +0200

    hjhj

--- Model/MeetingRoom.cs
+++ Model/MeetingRoom.cs
@@ -10,18 +10,22 @@
         public int Capacity { get; set; }
         public bool IsAvaliable { get; set; }
         public bool Whiteboard { get; set; }
-        public bool Projector   { get; set; }
+        public bool Projector { get; set; }
         public string ImagePath { get; set; }
 
         //constructer
-        public MeetingRoom(int id, string name, int capacity, bool isavailable,string equipment, string imagepath)
+        public MeetingRoom(int id, string name, int capacity, bool isavailable, bool whiteboard, bool projector, string imagepath)
         {
-            ID = id;
+            Id = id;
             Name = name;
-            Description = description;
+            Capacity = capacity;
+            IsAvailable = isavailable;
+            Whiteboard = whiteboard;
+            Projector = projector;
             ImagePath = imagepath;
-            Size = size;
         }
-        sqcdwwe
-
+    }
 }
+        
+
+     
