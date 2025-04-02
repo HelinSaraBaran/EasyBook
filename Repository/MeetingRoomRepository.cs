49fa6cd96777e49e14d16241c5c6cb37619e42d5
FL03NG <sih013@edu.zealand.dk>
2025-04-02 13:09:34 +0200

    Meeting room goodies

--- Repository/MeetingRoomRepository.cs
+++ Repository/MeetingRoomRepository.cs
@@ -13,14 +13,11 @@ namespace EasyBook.Repository
             new MeetingRoom(1, "Mødelokale A", 10, true, true, true, "../photos/lokal1.jpg"),
             new MeetingRoom(2, "Mødelokale B", 20, true, true, true, "../photos/lokal2.jpg"),
             new MeetingRoom(3, "Mødelokale C", 30, true, true, true, "../photos/lokal3.jpg"),
-            new MeetingRoom(1, "Mødelokale A", 10, true, true, true, "../photos/lokal4.jpg"),
-            new MeetingRoom(2, "Mødelokale B", 20, true, true, true, "../photos/lokal5.jpg"),
-            new MeetingRoom(3, "Mødelokale C", 30, true, true, true, "../photos/lokal6.jpg"),
+            new MeetingRoom(4, "Mødelokale D", 10, true, true, true, "../photos/lokal4.jpg"),
+            new MeetingRoom(5, "Mødelokale E", 20, true, true, true, "../photos/lokal5.jpg"),
+            new MeetingRoom(6, "Mødelokale F", 30, true, true, true, "../photos/lokal6.jpg"),
         };
-
-        new MeetingRoom { Id = 3, Name = "Mødelokale C", Capacity = 30, Equipment = "Projektor",
-            IsAvailable = true }
-        };
+        
         // metode til at hente alle mødelokalerne
         public List<MeetingRoom> GetAll() => meetingRooms;
     }
