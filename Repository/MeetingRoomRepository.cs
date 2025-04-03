5ffbbf6b52a8bd24f9221ec75e6ce9328a6638ba
Ahmed Hemdan <aha007@edu.zealand.dk>
2025-04-03 10:50:07 +0200

    changes

--- Repository/MeetingRoomRepository.cs
+++ Repository/MeetingRoomRepository.cs
@@ -10,11 +10,11 @@ namespace EasyBook.Repository
         /// </summary>
         private List<MeetingRoom> meetingRooms = new List<MeetingRoom>
         {
-            new MeetingRoom(1, "Mødelokale A", 10, true, true, true, "../photos/lokal1.jpg"),
-            new MeetingRoom(2, "Mødelokale B", 20, false, true, true, "../photos/lokal2.jpg"),
-            new MeetingRoom(3, "Mødelokale C", 30, true, true, true, "../photos/lokal3.jpg"),
-            new MeetingRoom(4, "Mødelokale D", 10, true, true, true, "../photos/lokal4.jpg"),
-            new MeetingRoom(5, "Mødelokale E", 20, true, true, true, "../photos/lokal5.jpg"),
+            new MeetingRoom(1, "Mødelokale A", 20, true, true, false, "../photos/lokal1.jpg"),
+            new MeetingRoom(2, "Mødelokale B", 25, false, true, true, "../photos/lokal2.jpg"),
+            new MeetingRoom(3, "Mødelokale C", 10, true, true, false, "../photos/lokal3.jpg"),
+            new MeetingRoom(4, "Mødelokale D", 15, false, false, true, "../photos/lokal4.jpg"),
+            new MeetingRoom(5, "Mødelokale E", 20, true, false, true, "../photos/lokal5.jpg"),
             new MeetingRoom(6, "Mødelokale F", 30, true, true, true, "../photos/lokal6.jpg"),
         };
         
