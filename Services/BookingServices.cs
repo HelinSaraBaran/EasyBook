66a8b07febd754f8c1acc931bf8e1c0b663d4b6c
FL03NG <sih013@edu.zealand.dk>
2025-04-04 10:37:35 +0200

    :)

--- Services/BookingServices.cs
+++ Services/BookingServices.cs
@@ -1,30 +1,30 @@
 ﻿using EasyBook.Model;
 using EasyBook.Repository;
 
-namespace EasyBook.Services
-{
+//namespace EasyBook.Services
+//{
     //public class BookingServices
     //{
-      //  private BookingRepository _bookingREPO;
+    //    private BookingRepository _bookingREPO;
 
-       // //public BookingServices(BookingRepository bookingREPO) 
-       // {
-        //    _bookingREPO = bookingREPO;
-        //}
-        //public void add(Booking booking) 
-       // {
-       //     _bookingREPO.Add(booking);
-       // }
-        //public List<Booking> GetAll()
-       // {
-      //      return _bookingREPO.Getall();
-       // }
-       // public Booking Get(int id)
-       // {
-       //     return _bookingREPO.Get(id);
-       // }
+    // //public BookingServices(BookingRepository bookingREPO) 
+    // {
+    //        _bookingREPO = bookingREPO;
+    //    }
+    //    public void add(Booking booking)
+    //    {
+    //        _bookingREPO.Add(booking);
+    //    }
+    //    public List<Booking> GetAll()
+    //    {
+    //        return _bookingREPO.Getall();
+    //    }
+    //    public Booking Get(int id)
+    //    {
+    //        return _bookingREPO.Get(id);
+    //    }
 
 
 
-    }
+    //}
 
